387eea029f768b6945df1a8b42a48e60a450e57d
Ovags <anxious.thoughts@gmail.com>
2021-12-16 18:42:17 +0100

    Coded exercise 05 to pass 4/6 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    if(numberOne<0||numberTwo<0){
+        return 'ERROR';
+    }
+    let small;
+    let big;
+    let sum=0;
+    if (numberOne<numberTwo){
+        small=numberOne;
+        big=numberTwo;
+    }else {
+        small=numberTwo;
+        big=numberOne;
+    }
+    for(i=small;i<big+1;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
